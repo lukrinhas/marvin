fbd4e8732a297412dc13e9f12c7c37615ad5014c
lukrinhas <lucas.suddenbr@hotmail.com>
2020-02-11 11:03:44 -0300

    marvin

---
+++ fase04/ex01/ateZero.js
@@ -0,0 +1,17 @@
+  
+function ateZero(numero){
+    var n = 0
+    
+        while(n <= numero){
+        console.log(n)
+        n++
+        
+        }
+    
+    if(numero <= 0) {
+        while(n >= numero){
+        console.log(numero)
+        numero++
+        }
+    }
+}
