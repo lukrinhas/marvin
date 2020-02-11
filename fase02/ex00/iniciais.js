bfc0042c0eaf0444121647c0473ad54209c84b40
lukrinhas <lucas.suddenbr@hotmail.com>
2020-02-11 09:07:17 -0300

    marvin

--- fase02/ex00/iniciais.js
+++ fase02/ex00/iniciais.js
@@ -1,4 +1,4 @@
-function iniciais(palavra) {
-   console.log(iniciais("mochileiro")) 
+function maisculo (palavra){
+    const palavras = palavra
+    return palavra.substring(0,3).toUpperCase().concat(palavra.substring(3))
 }
-
