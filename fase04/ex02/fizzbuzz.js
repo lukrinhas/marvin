5d2c969291316a5ea702c97a9de926c3f021daa5
lukrinhas <lucas.suddenbr@hotmail.com>
2020-02-11 11:18:39 -0300

    marvin

--- fase04/ex02/fizzbuzz.js
+++ fase04/ex02/fizzbuzz.js
@@ -3,15 +3,15 @@ function fizzbuzz(num1, num2){
     while (num1 <= num2){
         
         if (num1 %3 === 0 && num1 % 5 === 0){
-            console.log(num1 + " fizzbuzz");
+            console.log(num1 + " Fizzbuzz");
             num1++
         }
         if (num1 % 3 === 0){
-            console.log(num1 + "fizz");
+            console.log(num1 + " Fizz");
             num1++
         }
         if (num1 %5 === 0){
-            console.log(num1 + "buzz");
+            console.log(num1 + " Buzz");
             num1++
         }
         console.log(num1)
