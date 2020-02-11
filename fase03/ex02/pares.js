b02e63a85e47bef703f6295c2ae34626e02a5640
lukrinhas <lucas.suddenbr@hotmail.com>
2020-02-11 10:39:03 -0300

    marvin

---
+++ fase03/ex02/pares.js
@@ -0,0 +1,11 @@
+function pares(num1, num2) {
+    var resto1 = num1 % 2;
+    var resto2 = num2 % 2;
+    var total = resto1 || resto2;
+    
+    if(total === 0) {
+        return "Os números são pares"
+    }else {
+       return "Os números não são pares"
+    }
+}
